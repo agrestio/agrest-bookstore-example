996f0c52986aa62f1ce42c08cf9ff894aaaee3dc
Andrus Adamchik <andrus@objectstyle.com>
2019-07-29 11:18:30 -0400

    adding invalidation

--- src/main/java/org/example/agrest/persistent/Category.java
+++ src/main/java/org/example/agrest/persistent/Category.java
@@ -1,7 +1,9 @@
 package org.example.agrest.persistent;
 
+import org.apache.cayenne.cache.invalidation.CacheGroups;
 import org.example.agrest.persistent.auto._Category;
 
+@CacheGroups("categories")
 public class Category extends _Category {
 
 }
