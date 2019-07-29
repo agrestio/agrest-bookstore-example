eeb9d0014a7600b0b2e39928ddbf41e33af62135
Andrus Adamchik <andrus@objectstyle.com>
2019-07-29 11:23:33 -0400

    explicit cache policies

--- src/main/java/org/example/agrest/BookstoreApplication.java
+++ src/main/java/org/example/agrest/BookstoreApplication.java
@@ -3,19 +3,23 @@ package org.example.agrest;
 import io.agrest.runtime.AgBuilder;
 import io.agrest.runtime.AgRuntime;
 import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.jcache.JCacheModule;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
+import java.net.URL;
 
 @ApplicationPath("/api/*")
 public class BookstoreApplication extends ResourceConfig {
 
     public BookstoreApplication() {
 
-        ServerRuntime cayenneRuntime
-                = ServerRuntime.builder()
-                               .addConfig("cayenne-project.xml")
-                               .build();
+        URL config = getClass().getClassLoader().getResource("ehcache.xml");
+
+        ServerRuntime cayenneRuntime = ServerRuntime.builder()
+                .addConfig("cayenne-project.xml")
+                .addModule(b -> JCacheModule.contributeJCacheProviderConfig(b, config.toString()))
+                .build();
 
         AgRuntime agRuntime = AgBuilder.build(cayenneRuntime);
         super.register(agRuntime);
